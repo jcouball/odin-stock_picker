a853e09f9329c0a5c1bf40cff00b4f93f50eec60
James Couball <jcouball@yahoo.com>
2025-09-05 09:16:26 -0700

    Newly generated project

---
+++ lib/odin/stock_picker.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "stock_picker/version"
+
+module Odin
+  module StockPicker
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
