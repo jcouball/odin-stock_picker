bc1c9915228aff9e26506ba52eae4456c1c6d3c9
James Couball <jcouball@yahoo.com>
2025-09-05 15:02:36 -0700

    Add specs to test the desired implementation

--- spec/odin/stock_picker_spec.rb
+++ spec/odin/stock_picker_spec.rb
@@ -4,4 +4,47 @@ RSpec.describe Odin::StockPicker do
   it 'has a version number' do
     expect(Odin::StockPicker::VERSION).not_to be nil
   end
+
+  describe '#stock_picker' do
+    subject { described_class.stock_picker(stock_prices) }
+
+    context 'with no days' do
+      let(:stock_prices) { [] }
+      it { is_expected.to eq(nil) }
+    end
+
+    context 'with one day' do
+      let(:stock_prices) { [15] }
+      it { is_expected.to eq(nil) }
+    end
+
+    context 'with two days' do
+      let(:stock_prices) { [15, 20] }
+      it { is_expected.to eq([0, 1]) }
+    end
+
+    context 'when the best recommendation breaks even' do
+      context 'with two stock prices' do
+        let(:stock_prices) { [20, 20] }
+        it { is_expected.to eq(nil) }
+      end
+    end
+
+    context 'when the best recommendation loses money' do
+      context 'with two stock prices' do
+        let(:stock_prices) { [20, 15] }
+        it { is_expected.to eq(nil) }
+      end
+
+      context 'with many stock prices' do
+        let(:stock_prices) { [20, 19, 15, 14, 10, 5, 3, 2, 1] }
+        it { is_expected.to eq(nil) }
+      end
+    end
+
+    context 'with ten days' do
+      let(:stock_prices) { [17, 3, 6, 9, 15, 8, 6, 1, 10, 12] }
+      it { is_expected.to eq [1, 4] }
+    end
+  end
 end
